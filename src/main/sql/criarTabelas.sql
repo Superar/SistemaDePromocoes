71bbeb5f48e43953f7a5b31a3136bf1e40f92a66
Marcio Lima <marcio.lima.inacio@gmail.com>
2018-04-16 15:38:35 -0300

    Alteração na criação de tabelas
    - Criação da tabela de Administrador
    - Requerimento de todos os atributos necessários

--- src/main/sql/criarTabelas.sql
+++ src/main/sql/criarTabelas.sql
@@ -1,13 +1,14 @@
 DROP TABLE Promocao;
 DROP TABLE Site;
 DROP TABLE Hotel;
+DROP TABLE Administrador;
 
 CREATE TABLE Site
 (
 	    url VARCHAR(256) NOT NULL,
-	    nome VARCHAR(256),
-	    senha VARCHAR(20),
-	    telefone VARCHAR(24),
+	    nome VARCHAR(256) NOT NULL,
+	    senha VARCHAR(20) NOT NULL,
+	    telefone VARCHAR(24) NOT NULL,
 	    
 	    CONSTRAINT primary_key_site PRIMARY KEY (url)
 );
@@ -15,9 +16,9 @@ CREATE TABLE Site
 CREATE TABLE Hotel
 (
 	    CNPJ VARCHAR(14) NOT NULL,
-	    nome VARCHAR(256),
-	    senha VARCHAR(20),
-	    cidade VARCHAR(256),
+	    nome VARCHAR(256) NOT NULL,
+	    senha VARCHAR(20) NOT NULL,
+	    cidade VARCHAR(256) NOT NULL,
 
 	    CONSTRAINT primary_key_hotel PRIMARY KEY (cnpj)
 );
@@ -26,9 +27,17 @@ CREATE TABLE Promocao
 (
 	    urlSite VARCHAR(256) NOT NULL REFERENCES Site (url),
 	    CNPJHotel VARCHAR(14) NOT NULL REFERENCES Hotel (CNPJ),
-	    preco REAL,
-	    dataInicial DATE,
-	    dataFinal DATE,
+	    preco REAL NOT NULL,
+	    dataInicial DATE NOT NULL,
+	    dataFinal DATE NOT NULL,
 
 	    CONSTRAINT primary_key_promocao PRIMARY KEY (urlSite, CNPJHotel)
+);
+
+CREATE TABLE Administrador
+(
+            login VARCHAR(30) NOT NULL,
+            senha VARCHAR(20) NOT NULL,
+
+            CONSTRAINT primary_key_administrador PRIMARY KEY (login)
 );
