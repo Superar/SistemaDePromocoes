60dc193cab6740fae4321e8c6d06b2a0f77d113c
Marcio Lima <marcio.lima.inacio@gmail.com>
2018-04-20 18:33:03 -0300

    Filtro na listagem de promoções

--- src/main/java/br/ufscar/dc/promocoes/dao/PromocaoDAO.java
+++ src/main/java/br/ufscar/dc/promocoes/dao/PromocaoDAO.java
@@ -28,6 +28,19 @@ public class PromocaoDAO {
             + " INNER JOIN PROMOCOES.HOTEL AS H ON H.CNPJ = P.CNPJHOTEL"
             + " WHERE CNPJHOTEL = ?";
 
+    private final static String LISTAR_TODAS_PROMOCOES_POR_SITE_SQL = "SELECT"
+            + " P.URLSITE, P.CNPJHOTEL, P.PRECO, P.DATAINICIAL, P.DATAFINAL,"
+            + " S.URL, S.NOME, S.SENHA, S.TELEFONE,"
+            + " H.CNPJ, H.NOME, H.SENHA, H.CIDADE"
+            + " FROM PROMOCOES.PROMOCAO AS P"
+            + " INNER JOIN PROMOCOES.SITE AS S ON S.URL = P.URLSITE"
+            + " INNER JOIN PROMOCOES.HOTEL AS H ON H.CNPJ = P.CNPJHOTEL"
+            + " WHERE P.URLSITE = ?";
+    
+    private final static String LISTAR_TODAS_PROMOCOES_FILTRO = LISTAR_TODAS_PROMOCOES_POR_HOTEL_SQL
+            + " UNION "
+            + LISTAR_TODAS_PROMOCOES_POR_SITE_SQL;
+
     DataSource dataSource;
 
     public PromocaoDAO(DataSource dataSource) {
@@ -47,35 +60,36 @@ public class PromocaoDAO {
         return promocao;
     }
 
-    public List<Promocao> listarTodasPromocoesPorHotel(String hotelCNPJ) throws SQLException, NamingException {
+    public List<Promocao> listarTodasPromocoesFiltro(String hotelCNPJ, String siteURL) throws SQLException, NamingException {
         List<Promocao> ret = new ArrayList<>();
         try (Connection con = dataSource.getConnection();
                 PreparedStatement ps = con.prepareStatement(LISTAR_TODAS_PROMOCOES_POR_HOTEL_SQL)) {
 
             ps.setString(1, hotelCNPJ);
+            ps.setString(2, siteURL);
 
             try (ResultSet rs = ps.executeQuery()) {
                 while (rs.next()) {
                     Promocao promocao = new Promocao();
                     Site site = new Site();
                     Hotel hotel = new Hotel();
-                    
+
                     site.setNome(rs.getString("S.NOME"));
                     site.setSenha(rs.getString("S.SENHA"));
                     site.setTelefone(rs.getString("S.TELEFONE"));
                     site.setUrl(rs.getString("S.URL"));
-                    
+
                     hotel.setCNPJ(rs.getString("H.CNPJ"));
                     hotel.setCidade(rs.getString("H.CIDADE"));
                     hotel.setNome(rs.getString("H.NOME"));
                     hotel.setSenha(rs.getString("H.SENHA"));
-                    
+
                     promocao.setSite(site);
                     promocao.setHotel(hotel);
                     promocao.setPreco(rs.getDouble("P.PRECO"));
                     promocao.setDataInicial(rs.getTimestamp("P.DATAINICIAL"));
                     promocao.setDataFinal(rs.getTimestamp("P.DATAFINAL"));
-                    
+
                     ret.add(promocao);
                 }
             }
