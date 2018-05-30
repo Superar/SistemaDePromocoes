1444316abef054ea045a4cadb2ac1ad09a2d0760
Sylviane Vitor <sylvianevitor@gmail.com>
2018-05-29 21:33:52 -0300

    Bean gerenciado NovoHotel

---
+++ src/main/java/br/ufscar/dc/promocoes/views/NovoHotel.java
@@ -0,0 +1,69 @@
+package br.ufscar.dc.promocoes.views;
+
+import br.ufscar.dc.promocoes.beans.Hotel;
+import br.ufscar.dc.promocoes.dao.HotelDAO;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.naming.NamingException;
+import java.io.Serializable;
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@Named
+@SessionScoped
+public class NovoHotel implements Serializable {
+    Hotel dadoHotel;
+
+    @Inject
+    HotelDAO hotelDAO;
+
+    @Inject
+    Auth auth;
+
+    @Inject
+    MensagemHandler handler;
+
+    public NovoHotel() {
+        dadoHotel = new Hotel();
+    }
+
+    public Hotel getDadoHotel() {
+        return dadoHotel;
+    }
+
+    public void setDadoHotel(Hotel dadoHotel) {
+        this.dadoHotel = dadoHotel;
+    }
+
+    public Boolean validar_CNPJ(Hotel dadoHotel){
+        if(dadoHotel.getCNPJ().length() != 14){
+            handler.setMensagem(true, "CNPJ não compatível", MensagemHandler.TipoMensagem.TIPO_ERRO);
+            return false;
+        } else if(!dadoHotel.getCNPJ().matches("[0-9]+") ){
+            handler.setMensagem(true, "O CNPJ só pode conter números", MensagemHandler.TipoMensagem.TIPO_ERRO);
+            return false;
+        }
+        return true;
+    }
+
+    public String cadastrar() throws SQLException, NamingException {
+        if (auth.isAdmin()) {
+            if(validar_CNPJ(dadoHotel)) {
+                try {
+                    hotelDAO.gravarHotel(dadoHotel);
+                    handler.setMensagem(true, "Hotel cadastrado com sucesso", MensagemHandler.TipoMensagem.TIPO_SUCESSO);
+                } catch (SQLIntegrityConstraintViolationException e) {
+                    handler.setMensagem(true, "Hotel já cadastrado", MensagemHandler.TipoMensagem.TIPO_ERRO);
+                }
+
+            }
+            return "hotelForm";
+
+        } else {
+            handler.setMensagem(true, "<strong>ERRO 401</strong>: Permissão negada", MensagemHandler.TipoMensagem.TIPO_ERRO);
+            return "erro";
+        }
+    }
+}
